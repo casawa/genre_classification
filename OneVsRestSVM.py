a356b2b2f1967bc1a867a6aa6cce80ce655b98d4
Chaitanya Govind Asawa <casawa@corn20.stanford.edu>
2016-06-06 10:29:28 -0700

    Added print statements for baseline

--- OneVsRestSVM.py
+++ OneVsRestSVM.py
@@ -3,10 +3,13 @@ from sklearn.svm import LinearSVC
 from DataModel import DataModel
 
 def main():
+    print "Loading data..."
     data_model = DataModel()
     clf = OneVsRestClassifier(LinearSVC(random_state=0))
+    print "Training classifier..."
     clf.fit(data_model.train_X, data_model.train_y)
-    print clf.score(data_model.test_X, data_model.test_y)
+    print "Evaluating classifier..."
+    print "Average Test Accuracy", clf.score(data_model.test_X, data_model.test_y)
 
 if __name__ == '__main__':
     main()
